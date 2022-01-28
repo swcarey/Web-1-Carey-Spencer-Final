a0e00bdd83c90890d43d8a4c60f7fe6cbf46ab83
Spencer Carey <la.spencer.4@gmail.com>
2022-01-27 18:07:12 -0800

    Updating Homework

--- src/React/Homework/Essays/Essays.jsx
+++ src/React/Homework/Essays/Essays.jsx
@@ -8,13 +8,30 @@ const Essays = () => {
         <EssaysStyled>
             <h2>Essays</h2>
             <Essay q='S2. What is the difference between Git, Github and Heroku?'>
-                This is my answer
+                <ul>
+                    <li>Git: </li>
+                    <li>Github: </li>
+                    <li>Heroku: </li>
+                </ul>
             </Essay >
             <Essay q='S3. Explain the difference between HTML, CSS and JS.'>
-                Something else
+                <ul>
+                    <li>HTML: </li>
+                    <li>CSS: </li>
+                    <li>JavaScript: </li>
+                </ul>>
             </Essay>
             <Essay q='S4. What are Single Page Apps (SPAs)? How do they differ from traditional multi-page Web Sites?'>
-                Yet another
+                <ul>
+                    <li>Single Page Apps: </li>
+                    <li>Multi-page Apps: </li>
+                </ul>>
+            </Essay>
+            <Essay q='What is the difference between Web Designer, Front End Developer and Back End Developer?'>
+                <ul>
+                    <li>Web Designer: </li>
+                    <li>Front End Developer: </li>
+                </ul>>
             </Essay>
         </EssaysStyled>
     )
