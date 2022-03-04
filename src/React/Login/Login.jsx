100de0af63f62a05d1080e0e7028692a11635011
Spencer Carey <la.spencer.4@gmail.com>
2022-03-03 18:17:26 -0800

    Adding log in page

--- src/React/Login/Login.jsx
+++ src/React/Login/Login.jsx
@@ -1,11 +1,37 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+/* Componenets ---------------------------*/
+import UniversalForm, { Input, Textarea, SubmitButton } from 'React/common/UniversalForm/UniversalForm.jsx';
+
 const Login = () => {
+
+    const onSubmitHandler = ({fields, payload}) => {
+        console.log('onSubmit fields, payload', fields, payload);
+    }
 
     return (
         <LoginStyled className='Login'>
             <h1>Login</h1> 
+            <UniversalForm
+                apiUrl='/user/login'
+                onSubmit={ onSubmitHandler }
+            >
+                <Input 
+                    label='Username'
+                    id='username'
+                    placeholder='user@domain.com'
+                    rules={ ['required'] }
+                />
+                <Input 
+                    label='Password'
+                    id='password'
+                    type='password'
+                    placeholder='*****'
+                    rules={ ['required'] }
+                />
+                <SubmitButton>Log in</SubmitButton>
+            </UniversalForm>
         </LoginStyled>
     );
 }
